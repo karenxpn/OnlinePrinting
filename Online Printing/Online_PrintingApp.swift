a8e94426e130760fd404cac9ed6e085ed7d6b413
KALIMI <karenmirakyan@gmail.com>
2020-09-26 23:32:28 +0400

    Initial Commit

---
+++ Online Printing/Online_PrintingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_PrintingApp.swift
+//  Online Printing
+//
+//  Created by Karen Mirakyan on 26.09.20.
+//
+
+import SwiftUI
+
+@main
+struct Online_PrintingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
